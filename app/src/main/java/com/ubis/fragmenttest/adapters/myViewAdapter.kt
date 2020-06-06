3dfd6e49f3a831a39ee03e0dd6ae251bf4332ef7
nagasee0526 <roronalove@gmail.com>
2020-06-06 16:03:21 +0900

    페이지별 제목 추가

--- app/src/main/java/com/ubis/fragmenttest/adapters/myViewAdapter.kt
+++ app/src/main/java/com/ubis/fragmenttest/adapters/myViewAdapter.kt
@@ -7,6 +7,11 @@ import com.ubis.fragmenttest.fregments.firstFragment
 import com.ubis.fragmenttest.fregments.secondFragment
 
 class myViewAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if(position == 0) return "내 프로필"
+        else return "수강생 목록"
+    }
     override fun getItem(position: Int): Fragment {
         if(position == 0) return firstFragment()
         else return secondFragment()
